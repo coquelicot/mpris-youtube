d409b57d58de13b555a5aa9f3d4684a97efdc860
fcrh <coquelicot1117@gmail.com>
2013-02-14 17:16:35 +0800

    Many things...

--- MpYt.py
+++ MpYt.py
@@ -16,13 +16,22 @@
 # along with mpris-youtube.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import sys
 import datetime
 import httplib2
 from optparse import OptionParser
+
+import pyglet
+
 from apiclient.discovery import build
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.tools import run
 from oauth2client.keyring_storage import Storage
+
+import gobject
+import dbus
+import dbus.service
+import dbus.mainloop.glib
 
 class Struct:
 
@@ -110,10 +119,146 @@ class Cacher:
     def getFile(self, fileName):
         self.logger.warning('Cacher not implemented')
 
-class DBusInterface:
+class DBusInterface(dbus.service.Object):
+
+    NAME = "org.fcrh.MpYt"
+    PATH = "/org/mpris/MediaPlayer2"
+    IFACE_MAIN = "org.mpris.MediaPlayer2"
+    IFACE_PLAYER = "org.mpris.MediaPlayer2.Player"
+    #IFACE_PLAYLIST = "org.mpris.MediaPlayer2.Playlists"
+    IFACE_PROPERTY = "org.freedesktop.DBus.Properties"
 
     def __init__ (self, MpYt):
         self.MpYt = MpYt
+        self.player = MpYt.player
+        self.logger = Logger('DBusInterface')
+
+    # org.mpris.MediaPlayer2
+    @dbus.service.method(IFACE_MAIN)
+    def Raise(self):
+        raise RuntimeError("Don't have a gui yet.")
+
+    @dbus.service.method(IFACE_MAIN)
+    def Quit(self):
+        self.logger.info('Quit')
+        sys.exit(0)
+
+    # org.mpris.MediaPlayer2.Player
+    @dbus.service.method(IFACE_PLAYER)
+    def Next(self):
+        if self.player.props['CanGoNext']:
+            raise NotImplementedError('Next')
+
+    @dbus.service.method(IFACE_PLAYER)
+    def Previous(self):
+        if self.player.props['CanGoPrevious']:
+            raise NotImplementedError('Previous')
+
+    @dbus.service.method(IFACE_PLAYER)
+    def Pause(self):
+        if self.player.props['CanPause']:
+            raise NotImplementedError('Pause')
+
+    @dbus.service.method(IFACE_PLAYER)
+    def PlayPause(self):
+        if self.player.props['CanPlayPause']:
+            raise NotImplementedError('PlayPause')
+        else:
+            raise RuntimeError('Error')
+
+    @dbus.service.method(IFACE_PLAYER)
+    def Stop(self):
+        if self.player.props['CanControl']:
+            raise NotImplementedError('Stop')
+        else:
+            raise RuntimeError('Error')
+
+    @dbus.service.method(IFACE_PLAYER)
+    def Play(self):
+        if self.player.props['CanPlay']:
+            raise NotImplementedError('Play')
+
+    @dbus.service.method(IFACE_PLAYER, in_signature='x')
+    def Seek(self, offset):
+        if self.player.props['CanSeek']:
+            raise NotImplementedError('Seek')
+
+    @dbus.service.method(IFACE_PLAYER, in_signature='ox')
+    def SetPosition(self, trackId, position):
+        if self.player.props['CanSeek']:
+            raise NotImplementedError('SetPosition')
+
+    @dbus.service.method(IFACE_PLAYER, in_signature='s')
+    def OpenUri(self, uri):
+        raise RuntimeError('Error')
+
+    @dbus.service.signal(IFACE_PLAYER, signature='x')
+    def Seeked(self, position):
+        pass
+
+    # org.freedesktop.DBus.Properties
+    @dbus.service.method(IFACE_PROPERTY, in_signature='s', out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface == IFACE_MAIN:
+            return self.MpYt.props
+        elif interface == IFACE_PLAYER:
+            return self.player.props
+        else:
+            raise RuntimeError("Key Error")
+
+class UserInterface:
+
+    def __init__(self, MpYt):
+        self.MpYt = MpYt
+        self.player = MpYt.player
+
+class Player:
+
+    def __init__(self, MpYt):
+        self.MpYt = MpYt
+        self._player = pyglet.media.Player()
+        self.idx = -1
+        self.playlist = []
+
+        self.props = dict(
+                PlaybackStatus='Stopped', # Playing, Paused, Stopped
+                #LoopStatusk='None', # None, Track, Playlist
+                Rate=1.0, # only 1.0 for now
+                #Shuffle=False,
+                Metadata={},
+                Volume=1.0,
+                Position=0,
+                MinimumRate=1.0,
+                MaximumRate=1.0,
+                CanGoNext=False,
+                CanGoPrevious=False,
+                CanPlay=False,
+                CanPause=False,
+                CanSeek=False,
+                CanControl=True)
+    
+    def setPlaylist(self, playlist):
+        self.playlist = playlist
+        # TODO: update property
+
+    def play(self):
+        self._player.play()
+
+    def pause(self):
+        self._player.pause()
+
+    def seek(self, offset):
+        self._player.seek(offset)
+
+    def next(self):
+        self.idx += 1
+        if len(self.playlist) <= self.idx:
+            raise RuntimeError('No such song')
+
+    def prev(self):
+        self.idx -= 1
+        if self.idx < 0:
+            raise RuntimeError('No such song')
 
 class MprisYoutube:
 
@@ -122,8 +267,29 @@ class MprisYoutube:
         self.logger = Logger('MprisYoutube')
 
         self.loadConfig()
+        self.player = Player(self)
         self.cacher = Cacher(self.config)
+        self.userInterface = UserInterface(self)
         self.dbusInterface = DBusInterface(self)
+
+        self.props = dict(
+                CanQuit=True,
+                #FullScreen=False,
+                #CanSetFullscreen,
+                CanRaise=False,
+                HasTrackList=True,
+                Identity='mpris-youtube',
+                #DesktopEntry='What is this?',
+                SupportedUriSchemes=[], # can't open uri from outside
+                SupportedMimeTypes=['application/x-flash-video'])
+
+        """
+        for playlist in self.getLists():
+            print 'list %s:' % playlist["title"]
+
+            for item in self.getItems(playlist["id"], False):
+                print '\t%s (%s, %s)' % (item["title"], item["id"], item["videoId"])
+                """
 
     def loadConfig(self):
 
@@ -156,6 +322,57 @@ class MprisYoutube:
         except:
             self.logger.error("Can't save config.")
 
+    def getLists(self):
+
+        token = ""
+        result = []
+        youtube = APIService.instance(authenticate=True)
+
+        while True:
+            listResp = youtube.playlists().list(
+                    part="id,snippet,contentDetails",
+                    pageToken=token,
+                    maxResults=50,
+                    mine=True
+                    ).execute()
+            for item in listResp["items"]:
+                element = dict(
+                        id=item["id"],
+                        title=item["snippet"]["title"],
+                        description=item["snippet"]["description"],
+                        itemCount=item["contentDetails"]["itemCount"])
+                result.append(element)
+
+            try:
+                token = listResp["nextPageToken"]
+            except:
+                return result
+
+    def getItems(self, playlistId, authenticate=True):
+
+        token = ""
+        result = []
+        youtube = APIService.instance(authenticate=authenticate)
+
+        while True:
+            itemResp = youtube.playlistItems().list(
+                    part="id,snippet",
+                    pageToken=token,
+                    maxResults=50,
+                    playlistId=playlistId
+                    ).execute()
+            for item in itemResp["items"]:
+                element = dict(
+                        id=item["id"],
+                        title=item["snippet"]["title"],
+                        videoId=item["snippet"]["resourceId"]["videoId"])
+                result.append(element)
+
+            try:
+                token = itemResp["nextPageToken"]
+            except:
+                return result
+
 def main():
 
     parser = OptionParser()
@@ -169,3 +386,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
